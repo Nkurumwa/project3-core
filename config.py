8ebba2d581681edc6126484ddfcb3fee4fa449e5
Nkurumwa <nkurumwa51@gmail.com>
2019-10-25 13:00:11 +0300

    update config

--- config.py
+++ config.py
@@ -18,11 +18,7 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:ishmael@localhost/pitch'
-
 class ProdConfig(Config):
-    pass
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
