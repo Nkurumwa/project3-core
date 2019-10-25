0fa1494a7d4c1350a86b2ff8aeb95caffbab38c0
Nkurumwa <nkurumwa51@gmail.com>
2019-10-25 14:10:34 +0300

    update file

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export MAIL_USERNAME=nkurumwa51@gmail.com
-export MAIL_PASSWORD=ishmael51
-export SECRET_KEY=ishmael
+export MAIL_USERNAME='nkurumwa51@gmail.com'
+export MAIL_PASSWORD='ishmael51'
+export SECRET_KEY='ishmael'
 
 python3.6 manage.py server
